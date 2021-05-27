0b9dc4f5b029018877d1ef56bc5b8b476c6ffe5b
Jibank21 <68716761+Jibank21@users.noreply.github.com>
2021-05-28 09:26:53 +1000

    Update localhost.sql

--- localhost.sql
+++ localhost.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 04, 2019 at 01:02 AM
--- Server version: 5.7.23
--- PHP Version: 7.1.26
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2021 at 05:44 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `heartycook`
 --
-CREATE DATABASE IF NOT EXISTS `heartycook` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `heartycook`;
 
 -- --------------------------------------------------------
 
@@ -46,34 +43,6 @@ INSERT INTO `admin` (`admin_id`, `admin_username`, `fld_password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `food`
---
-
-CREATE TABLE `food` (
-  `food_id` int(11) NOT NULL,
-  `cook_id` int(11) NOT NULL,
-  `foodname` varchar(100) NOT NULL,
-  `cost` bigint(15) NOT NULL,
-  `cuisines` varchar(50) NOT NULL,
-  `paymentmode` varchar(50) NOT NULL,
-  `fldimage` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `food`
---
-
-INSERT INTO `food` (`food_id`, `cook_id`, `foodname`, `cost`, `cuisines`, `paymentmode`, `fldimage`) VALUES
-(2, 22, 'malai Kofta', 50, 'vegetable,veg', 'COD', '1469258122-malai-kofta.jpg'),
-(3, 22, 'shahi panner', 20, 'vegetable', 'COD', 'Shahi-Paneer-Recipe.jpg'),
-(4, 22, 'chola kulcha', 100, 'lunch', 'COD', 'maxresdefault.jpg'),
-(5, 23, 'Pizza', 100, 'Medium Size, fast food', 'COD', 'phut_0.jpg'),
-(6, 23, 'Pizza Full', 300, 'Fast food,full size', 'COD', 'phut_0.jpg'),
-(7, 23, 'burger ', 50, 'Fast food', 'COD', 'photo-1534790566855-4cb788d389ec.jpg');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `cart`
 --
 
@@ -82,6 +51,40 @@ CREATE TABLE `cart` (
   `product_id` bigint(11) NOT NULL,
   `customer_id` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `product_id`, `customer_id`) VALUES
+(11, 6, 'jeevan@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cook`
+--
+
+CREATE TABLE `cook` (
+  `cook_id` int(10) NOT NULL,
+  `cust_name` varchar(30) NOT NULL,
+  `cust_email` varchar(50) NOT NULL,
+  `fld_password` varchar(50) NOT NULL,
+  `fld_mob` bigint(10) NOT NULL,
+  `fld_phone` bigint(10) NOT NULL,
+  `fld_address` varchar(50) NOT NULL,
+  `pro_image` varchar(250) NOT NULL,
+  `email_verification` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cook`
+--
+
+INSERT INTO `cook` (`cook_id`, `cust_name`, `cust_email`, `fld_password`, `fld_mob`, `fld_phone`, `fld_address`, `pro_image`, `email_verification`) VALUES
+(22, 'Anish Panta', 'cook1@gmail.com', 'cook1', 7503515386, 114565457, 'noida', '46388969.jpg', 0),
+(23, 'Sagar Sapkota', 'cook2@gmail.com', 'cook2', 7503515385, 114565457, 'C-33, SWARN PARK, MUNDKA', '46388969.jpg', 0),
+(24, 'jeevan', 'jeevan@gmail.com', 'jeevan1', 9807331986, 9807331986, 'campsie', '9c935e14be80fcfa4233736a7d3d5a26.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -104,15 +107,52 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`cust_id`, `cust_name`, `cust_email`, `cust_phone`, `password`) VALUES
 (1, 'gajender', 'customer1@gmail.com', 7503515382, 'customer1'),
 (2, 'sanjay', 'customer2@gmail.com', 7503515386, 'customer2'),
-(3, 'saana', 'customer3@gmail.com', 7503515383, 'customer3');
+(3, 'saana', 'customer3@gmail.com', 7503515383, 'customer3'),
+(4, 'jeevan', 'jeevan@gmail.com', 9807331986, 'jeevan');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblfood`
+-- Table structure for table `delivery_person`
 --
--- Error reading structure for table heartycook.tblfood: #1033 - Incorrect information in file: '.\heartycook\tblfood.frm'
--- Error reading data for table heartycook.tblfood: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'FROM `heartycook`.`tblfood`' at line 1
+
+CREATE TABLE `delivery_person` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `food_id` int(11) NOT NULL,
+  `cook_id` int(11) NOT NULL,
+  `foodname` varchar(100) NOT NULL,
+  `cost` bigint(15) NOT NULL,
+  `cuisines` varchar(50) NOT NULL,
+  `paymentmode` varchar(50) NOT NULL,
+  `fldimage` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`food_id`, `cook_id`, `foodname`, `cost`, `cuisines`, `paymentmode`, `fldimage`) VALUES
+(2, 22, 'malai Kofta', 16, 'vegetable,veg', 'COD', '1469258122-malai-kofta.jpg'),
+(3, 22, 'shahi panner', 20, 'vegetable', 'COD', 'Shahi-Paneer-Recipe.jpg'),
+(4, 22, 'chola kulcha', 16, 'lunch', 'COD', 'maxresdefault.jpg'),
+(5, 23, 'Pizza', 15, 'Medium Size, fast food', 'COD', 'phut_0.jpg'),
+(6, 23, 'Pizza Full', 20, 'Fast food,full size', 'COD', 'phut_0.jpg'),
+(7, 23, 'burger ', 18, 'Fast food', 'COD', 'photo-1534790566855-4cb788d389ec.jpg'),
+(8, 24, 'Mushroom Bolognese', 19, 'mushroom', 'Online Payment', 'MushroomBolognese.jpg');
 
 -- --------------------------------------------------------
 
@@ -131,10 +171,10 @@ CREATE TABLE `message` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `order_id` int(10) NOT NULL,
   `cart_id` bigint(10) NOT NULL,
   `cook_id` bigint(10) DEFAULT NULL,
@@ -145,37 +185,16 @@ CREATE TABLE `order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `orders`
 --
 
-INSERT INTO `order` (`order_id`, `cart_id`, `cook_id`, `food_id`, `cust_email_id`, `payment`, `fldstatus`) VALUES
-(1, 1, 21, 1, 'customer3@gmail.com', '50', 'Delivered'),
-(2, 2, 22, 3, 'customer3@gmail.com', '20', 'Out Of Stock');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cook`
---
-
-CREATE TABLE `cook` (
-  `cook_id` int(10) NOT NULL,
-  `cust_name` varchar(30) NOT NULL,
-  `cust_email` varchar(50) NOT NULL,
-  `fld_password` varchar(50) NOT NULL,
-  `fld_mob` bigint(10) NOT NULL,
-  `fld_phone` bigint(10) NOT NULL,
-  `fld_address` varchar(50) NOT NULL,
-  `pro_image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `cook`
---
-
-INSERT INTO `cook` (`cook_id`, `cust_name`, `cust_email`, `fld_password`, `fld_mob`, `fld_phone`, `fld_address`, `pro_image`) VALUES
-(22, 'Hotel Radison', 'cook1@gmail.com', 'cook1', 7503515386, 114565457, 'noida', '46388969.jpg'),
-(23, 'Hotel Piccaso', 'cook2@gmail.com', 'cook2', 7503515385, 114565457, 'C-33, SWARN PARK, MUNDKA', '46388969.jpg');
+INSERT INTO `orders` (`order_id`, `cart_id`, `cook_id`, `food_id`, `cust_email_id`, `payment`, `fldstatus`) VALUES
+(2, 2, 22, 3, 'customer3@gmail.com', '20', 'Delivered'),
+(3, 6, 22, 3, 'customer1@gmail.com', '20', 'Delivered'),
+(4, 7, 23, 7, 'customer3@gmail.com', '18', 'In Process'),
+(5, 8, 22, 3, 'customer3@gmail.com', '20', 'In Process'),
+(6, 9, 22, 3, 'jeevan@gmail.com', '20', 'In Process'),
+(7, 10, 24, 8, 'jeevan@gmail.com', '19', 'In Process');
 
 --
 -- Indexes for dumped tables
@@ -188,17 +207,16 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `food`
---
-ALTER TABLE `food`
-  ADD PRIMARY KEY (`food_id`),
-  ADD KEY `cook_id` (`cook_id`);
-
---
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`);
+
+--
+-- Indexes for table `cook`
+--
+ALTER TABLE `cook`
+  ADD PRIMARY KEY (`cook_id`);
 
 --
 -- Indexes for table `customer`
@@ -207,22 +225,29 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`cust_id`);
 
 --
+-- Indexes for table `delivery_person`
+--
+ALTER TABLE `delivery_person`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `cook_id` (`cook_id`);
+
+--
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
   ADD PRIMARY KEY (`msg_id`);
 
 --
--- Indexes for table `order`
+-- Indexes for table `orders`
 --
-ALTER TABLE `order`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`);
-
---
--- Indexes for table `cook`
---
-ALTER TABLE `cook`
-  ADD PRIMARY KEY (`cook_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -235,22 +260,34 @@ ALTER TABLE `admin`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `food`
---
-ALTER TABLE `food`
-  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `cook`
+--
+ALTER TABLE `cook`
+  MODIFY `cook_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `delivery_person`
+--
+ALTER TABLE `delivery_person`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `message`
@@ -259,16 +296,10 @@ ALTER TABLE `message`
   MODIFY `msg_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `order`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `order`
-  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `cook`
---
-ALTER TABLE `cook`
-  MODIFY `cook_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+ALTER TABLE `orders`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -279,232 +310,6 @@ ALTER TABLE `cook`
 --
 ALTER TABLE `food`
   ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`cook_id`) REFERENCES `cook` (`cook_id`);
---
--- Database: `db_food`
---
-CREATE DATABASE IF NOT EXISTS `db_food` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `db_food`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
-  `admin_username` varchar(30) NOT NULL,
-  `fld_password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cook`
---
-
-CREATE TABLE `cook` (
-  `cook_id` int(10) NOT NULL,
-  `cust_name` varchar(30) NOT NULL,
-  `cust_email` varchar(50) NOT NULL,
-  `fld_password` varchar(30) NOT NULL,
-  `fld_mob` bigint(10) NOT NULL,
-  `fld_phone` bigint(10) NOT NULL,
-  `fld_address` varchar(100) NOT NULL,
-  `pro_image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_customer`
---
-
-CREATE TABLE `tbl_customer` (
-  `CustomerID` int(11) NOT NULL,
-  `CustomerName` varchar(250) NOT NULL,
-  `Address` text NOT NULL,
-  `City` varchar(250) NOT NULL,
-  `PostalCode` varchar(30) NOT NULL,
-  `Country` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_customer`
---
-
-INSERT INTO `tbl_customer` (`CustomerID`, `CustomerName`, `Address`, `City`, `PostalCode`, `Country`) VALUES
-(1, 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
-(2, 'Ana Trujillo', 'Avda. de la Construction 2222', 'Mexico D.F.', '5021', 'Mexico'),
-(3, 'Antonio Moreno', 'Mataderos 2312', 'Mexico D.F.', '5023', 'Mexico'),
-(4, 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
-(5, 'Paula Parente', 'Rua do Mercado, 12', 'Resende', '08737-363', 'Brazil'),
-(6, 'Wolski Zbyszek', 'ul. Filtrowa 68', 'Walla', '01-012', 'Poland'),
-(7, 'Matti Karttunen', 'Keskuskatu 45', 'Helsinki', '21240', 'Finland'),
-(8, 'Karl Jablonski', '305 - 14th Ave. S. Suite 3B', 'Seattle', '98128', 'USA'),
-(9, 'Paula Parente', 'Rua do Mercado, 12', 'Resende', '08737-363', 'Brazil'),
-(10, 'Pirkko Koskitalo', 'Torikatu 38', 'Oulu', '90110', 'Finland');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `cook`
---
-ALTER TABLE `cook`
-  ADD PRIMARY KEY (`cook_id`);
-
---
--- Indexes for table `tbl_customer`
---
-ALTER TABLE `tbl_customer`
-  ADD PRIMARY KEY (`CustomerID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cook`
---
-ALTER TABLE `cook`
-  MODIFY `cook_id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_customer`
---
-ALTER TABLE `tbl_customer`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- Database: `test`
---
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `test`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `OrderID` int(11) NOT NULL,
-  `OrderNumber` int(11) NOT NULL,
-  `PersonID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reg_data`
---
-
-CREATE TABLE `reg_data` (
-  `id` int(10) NOT NULL,
-  `name` text NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `address` varchar(70) NOT NULL,
-  `gender` enum('M','F') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `reg_data`
---
-
-INSERT INTO `reg_data` (`id`, `name`, `email`, `password`, `mobile`, `address`, `gender`) VALUES
-(1, 'ANISHA', '', '', '0', '', 'M'),
-(2, 'ANISHA', 'gajenderdh947@gmail.com', '', '0', '', 'M'),
-(3, 'ANISHA', 'gajenderdh947@gmail.com', '', '0', '', 'M'),
-(4, 'ANISHA', 'gajenderdh947@gmail.com', '', '0', '', 'M'),
-(5, 'ANISHA', 'gajenderdh947@gmail.com', 'dfdfdsdf', '0', '', 'M'),
-(6, 'ANISHA', 'gajenderdh947@gmail.com', 'dfdfdf', '7503128154', '', 'M'),
-(7, 'ANISHA', 'gajenderdh947@gmail.com', 'sdsdsd', '7503128154', 'MUNDKA', 'M'),
-(8, 'gajender', 'gajenderdh947539@outlook.com', 'fdfsd', '7503128154', 'MUNDKA', 'M');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_info`
---
-
-CREATE TABLE `user_info` (
-  `s.no` int(10) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user_info`
---
-
-INSERT INTO `user_info` (`s.no`, `username`, `password`) VALUES
-(1, '', ''),
-(2, 'gajenderdh947', '947'),
-(3, 'dahiya947', '947539');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`OrderID`),
-  ADD KEY `PersonID` (`PersonID`);
-
---
--- Indexes for table `reg_data`
---
-ALTER TABLE `reg_data`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_info`
---
-ALTER TABLE `user_info`
-  ADD PRIMARY KEY (`s.no`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `reg_data`
---
-ALTER TABLE `reg_data`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `user_info`
---
-ALTER TABLE `user_info`
-  MODIFY `s.no` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`PersonID`) REFERENCES `reg_data` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
